84e3e68b90aee9e3a2ed14b48fa2fc9d6e0ab45e
Vivek Kapoor <vkapoor18@icloud.com>
2023-04-29 23:55:59 +0000

    Developing raw tables for project and stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,24 @@
+select 
+-- from orders
+ORDERID ,
+	o.ORDERDATE ,
+	o.SHIPDATE ,
+	o.SHIPMODE ,
+    o.ORDERSELLINGPRICE,
+    o.ORDERCOSTPRICE,
+    o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as orderprofit,
+-- from customers
+    c.CUSTOMERID ,
+	c.CUSTOMERNAME ,
+	c.SEGMENT ,
+	c.COUNTRY ,
+ -- from products
+    p.CATEGORY ,
+	p.PRODUCTID ,
+	p.PRODUCTNAME ,
+	p.SUBCATEGORY    
+          from {{ ref('raw_orders') }} o
+          left join {{ ref('raw_customer') }} c
+          on c.CUSTOMERID = o.CUSTOMERID
+          left join {{ ref('raw_product') }} p
+          on p.PRODUCTID = o.PRODUCTID
