c6fd2239285671433845b0591a5c07e5bd8f8977
Vivek Kapoor <vkapoor18@icloud.com>
2023-04-30 20:08:01 +0000

    Added dbt_utils package, created seed and referenced in stg_orders model

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,5 @@
 select 
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders,
 -- from orders
 ORDERID ,
 	o.ORDERDATE ,
@@ -17,9 +18,13 @@ ORDERID ,
 	p.PRODUCTID ,
 	p.PRODUCTNAME ,
 	p.SUBCATEGORY ,
-    {{ markup('ORDERSELLINGPRICE','ORDERCOSTPRICE') }}   as markup
+    {{ markup('ORDERSELLINGPRICE','ORDERCOSTPRICE') }}   as markup,
+    d.delivery_team
           from {{ ref('raw_orders') }} o
           left join {{ ref('raw_customer') }} c
           on c.CUSTOMERID = o.CUSTOMERID
           left join {{ ref('raw_product') }} p
           on p.PRODUCTID = o.PRODUCTID
+          left join {{ ref('delivery_team') }} d
+          on o.SHIPMODE = d.SHIPMODE
+          
